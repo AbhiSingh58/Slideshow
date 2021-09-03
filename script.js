b83bf53904f36299f645ee8f5204b9a80c7f8908
Abhi Singh <88607051+AbhiSingh58@users.noreply.github.com>
2021-09-03 14:31:34 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,112 @@
+
+        const slide = document.querySelectorAll(".slideshow");
+        const circ = document.querySelectorAll(".circle")
+        const leftarrow = document.getElementById("left-arrow");
+        const rightarrow = document.getElementById("right-arrow");
+
+        document.addEventListener("keyup", function(e) {
+                
+           
+                        if (e.keyCode == 37) {
+                            console.log("hi")
+                            prevslide();
+                            slidechange()
+                            timerreset();
+                            circleupdate()
+                        }
+                         else if (e.keyCode == 39){
+
+                             console.log("tu bhi")
+                             nextslide();
+                             slidechange
+                             timerreset();
+                             circleupdate()
+                            }
+            })
+                         
+
+        
+
+
+
+
+
+        var currentslide = 0;
+
+        var timer = setInterval(slideshow, 000);
+        slideshow();
+
+        leftarrow.addEventListener("click", () => {
+            prevslide();
+            timerreset();
+            circleupdate()
+        })
+
+        rightarrow.addEventListener("click", () => {
+            nextslide();
+            timerreset();
+            circleupdate()
+
+        })
+
+
+        function prevslide() {
+            if (currentslide == 0) {
+                currentslide = slide.length - 1;
+
+            }
+            else {
+                currentslide--;
+            }
+            slidechange();
+        }
+
+
+
+
+        function nextslide() {
+            if (currentslide == slide.length - 1) {
+                currentslide = 0;
+            }
+            else {
+                currentslide++;
+            }
+            slidechange();
+        }
+
+        function slidechange() {
+            for (let i = 0; i < slide.length; i++) {
+                slide[i].classList.remove('visibility');
+
+            }
+            slide[currentslide].classList.add('visibility');
+        }
+
+        function circleupdate() {
+            for (let i = 0; i < slide.length; i++) {
+
+                circ[i].classList.remove('dot')
+            }
+            circ[currentslide].classList.add('dot')
+        }
+
+        function timerreset() {
+            clearInterval(timer);
+            timer = setInterval(slideshow, 5000);
+
+        }
+
+        function slideshow() {
+            nextslide();
+            timerreset();
+            circleupdate()
+        }
+
+
+
+        function circleIndex(n) {
+            currentslide = n;
+
+            nextslide()
+            circleupdate()
+        }
